bfea98b254c936c68dda8846acfc25fc404e4aea
Benjaminjunior <jardimjapao@gmail.com>
2019-05-24 07:12:33 -0300

    up

---
+++ SalvarProjetoGitEGithab.bat
@@ -0,0 +1,14 @@
+@echo 1 instale o git
+@echo 2 entre no diretorio
+@echo 3 criando o repositorio vazio 
+pause
+git init
+@echo 4 adicionando arquivo no repositorio vazio
+git add .
+pause
+@echo 5 adicionando comentario 
+git commit - m "comentario padrao"
+pause
+@echo 6 entre no github para crie um link de repositorio la 
+@echo execute o comando gerado 
+pause
